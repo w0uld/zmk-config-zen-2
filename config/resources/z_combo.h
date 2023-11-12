9a72b9c5573a0504ed1c510281aada237d7b965d
w0uld <49806545+w0uld@users.noreply.github.com>
2023-11-12 16:16:10 +0100

    Using modules

---
+++ config/resources/z_combo.h
@@ -0,0 +1,18 @@
+#include <behaviors.dtsi>
+#include <dt-bindings/zmk/keys.h>
+
+/* combos, idea from urob: https://github.com/urob/zmk-nodefree-config */
+#define ALL 0xff
+#define Z_COMBO(name, combo_bindings, keypos, combo_timeout, combo_prioridle, combo_layers) \
+  / { \
+        combos { \
+            compatible = "zmk,combos"; \
+            combo_ ## name { \
+                bindings = <combo_bindings>; \
+                key-positions = <keypos>; \
+                timeout-ms = <combo_timeout>; \
+                require-prior-idle-ms = <combo_prioridle>; \
+                layers = <combo_layers>;
+            }; \
+        }; \
+    };
---
+++ config/resources/z_modmorph.h
@@ -0,0 +1,18 @@
+#include <behaviors.dtsi>
+#include <dt-bindings/zmk/keys.h>
+
+/* mod-morph, idea from caksoylar: https://github.com/caksoylar */
+
+#define Z_ACCENT(name, regular_binding, morph_binding, morph_mods, keep_mods) \
+  / { \
+        behaviors { \
+            name: name { \
+                compatible = "zmk,behavior-mod-morph"; \
+                label = ZMK_MACRO_STRINGIFY(accent_ ## name); \
+                #binding-cells = <0>; \
+                bindings = <&kp regular_binding>, <&kp morph_binding>; \
+                mods = <(MOD_RALT)>; \
+                keep-mods = <keep_mods>; \
+            }; \
+        }; \
+    };
