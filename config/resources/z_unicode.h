34ecd59282c13690d6eb267eec907002f0d055f8
w0uld <49806545+w0uld@users.noreply.github.com>
2023-11-14 00:32:19 +0100

    Unicode support test

---
+++ config/resources/z_unicode.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <behaviors.dtsi>
+#include <dt-bindings/zmk/keys.h>
+
+/* unicode, copied from urob: https://github.com/urob/zmk-nodefree-config */
+
+/* ZMK_UNICODE */
+
+#if !defined OS_UNICODE_LEAD
+    #if HOST_OS == 2
+        #define OS_UNICODE_LEAD &macro_press &kp LALT      // macOS/Windows-Alt-Codes
+    #elif HOST_OS == 1
+        #define OS_UNICODE_LEAD &macro_tap &kp LS(LC(U))   // Linux
+    #else
+        #define OS_UNICODE_LEAD &macro_tap &kp RALT &kp U  // Windows + WinCompose (default)
+    #endif
+#endif
+#if !defined OS_UNICODE_TRAIL
+    #if HOST_OS == 2
+        #define OS_UNICODE_TRAIL &macro_release &kp LALT  // macOS/Windows-Alt-Codes
+    #elif HOST_OS == 1
+        #define OS_UNICODE_TRAIL &macro_tap &kp SPACE     // Linux
+    #else
+        #define OS_UNICODE_TRAIL &macro_tap &kp RET       // Windows + WinCompose (default)
+    #endif
+#endif
+
+#define UC_MACRO(name, unicode_bindings) \
+    / { \
+        macros { \
+            name: name { \
+                compatible = "zmk,behavior-macro"; \
+                label = ZMK_MACRO_STRINGIFY(UC_MACRO_ ## name); \
+                wait-ms = <0>; \
+                tap-ms = <0>; \
+                #binding-cells = <0>; \
+                bindings = <OS_UNICODE_LEAD>, <&macro_tap unicode_bindings>, <OS_UNICODE_TRAIL>; \
+            }; \
+        }; \
+    };
+
+#define UC_MODMORPH(name, uc_binding, shifted_uc_binding) \
+    / { \
+        behaviors { \
+            name: name { \
+                compatible = "zmk,behavior-mod-morph"; \
+                label = ZMK_MACRO_STRINGIFY(UC_MORPH_ ## name); \
+                #binding-cells = <0>; \
+                bindings = <uc_binding>, <shifted_uc_binding>; \
+                mods = <(MOD_LSFT|MOD_RSFT)>; \
+            }; \
+        }; \
+    };
+
+#define ZMK_UNICODE_SINGLE(name, L0, L1, L2, L3) \
+    UC_MACRO(name ## _lower, &kp L0 &kp L1 &kp L2 &kp L3) \
+    UC_MODMORPH(name, &name ## _lower, &none)
+
+#define ZMK_UNICODE_PAIR(name, L0, L1, L2, L3, U0, U1, U2, U3) \
+    UC_MACRO(name ## _lower, &kp L0 &kp L1 &kp L2 &kp L3) \
+    UC_MACRO(name ## _upper, &kp U0 &kp U1 &kp U2 &kp U3) \
+    UC_MODMORPH(name, &name ## _lower, &name ## _upper)
